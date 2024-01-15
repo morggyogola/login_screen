4683c29c7c221b44daed14e5ddd5d94bee59dc78
Morgan Ogola <morganogola@gmail.com>
2024-01-15 13:10:48 +0300

    added button and text widgets

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:login_app/widgets/button_widget.dart';
 import 'package:login_app/widgets/text_widget.dart';
 
 class LoginPage extends StatelessWidget {
@@ -40,10 +41,86 @@ class LoginPage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontSize: 15,
                   color: Colors.black,
-                  textAlign: TextAlign.center,
+                  textAlign: TextAlign.right,
                   fontFamily: "Monteserrat"),
             ],
-          )
+          ),
+          SizedBox(height: 50,),
+          Padding(
+            padding: EdgeInsets.only(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('\u270B'),
+                    TextWidget(
+                        text: " Welcome to Beamo",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black,
+                        textAlign: TextAlign.center,
+                        fontFamily: "Monteserrat"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 100,),
+          ButtonWidget(
+            widget: TextWidget(
+                text: "Login with email",
+                fontWeight: FontWeight.w500,
+                fontSize: 15,
+                color: Colors.black45,
+                textAlign: TextAlign.center,
+                fontFamily: "Montessarat"),
+            color: Colors.blue,
+            radius: 100,
+            width: 700,
+            borderColor: Colors.greenAccent,
+          ),
+          ButtonWidget(
+            widget: TextWidget(
+                text: "Login with google",
+                fontWeight: FontWeight.w500,
+                fontSize: 15,
+                color: Colors.black45,
+                textAlign: TextAlign.center,
+                fontFamily: "Montessarat"),
+            color: Colors.red,
+            radius: 100,
+            width: 700,
+            borderColor: Colors.greenAccent,
+          ),
+          ButtonWidget(
+            widget: TextWidget(
+                text: "Login with Facebook",
+                fontWeight: FontWeight.w500,
+                fontSize: 15,
+                color: Colors.black45,
+                textAlign: TextAlign.center,
+                fontFamily: "Montessarat"),
+            color: Colors.blue,
+            radius: 100,
+            width: 700,
+            borderColor: Colors.greenAccent,
+          ),
+          ButtonWidget(
+            widget: TextWidget(
+                text: "Next",
+                fontWeight: FontWeight.w500,
+                fontSize: 15,
+                color: Colors.black45,
+                textAlign: TextAlign.center,
+                fontFamily: "Montessarat"),
+            color: Colors.blue,
+            radius: 100,
+            width: 700,
+            borderColor: Colors.greenAccent,
+          ),
+
         ],
       ),
     ));
--- lib/widgets/button_widget.dart
+++ lib/widgets/button_widget.dart
@@ -2,17 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ButtonWidget extends StatelessWidget {
-  
-  final String text;
-  final Color bgColor;
-  final Color txtColor;
-  
-  const ButtonWidget({super.key, required this.text, required this.bgColor, required this.txtColor});
+
+  final Widget widget;
+  final Color color ;
+  final double radius;
+  final double width;
+  final Color borderColor;
+
+  const ButtonWidget({super.key,  required this.widget, required this.color, required this.radius, required this.width, required this.borderColor});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return Container(
+      width: width,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(radius),
+      ),
+      child: Padding(padding: const EdgeInsets.all(8.0),
+        child: widget,
+
+      ),
     );
   }
 }
